9dd0e1d8ad43b685a1384203b78c0bd05d09f8f3
Nikola Dordevic <nikoladjor@gmail.com>
2023-02-21 13:09:30 +0100

    added boilerplate for database migration and upgrade

---
+++ database_utils.sh
@@ -0,0 +1,5 @@
+# This is a placeholder at the moment, should be moved as CLI command
+
+# TODO: Create CLI commands
+flask --app dog_classifier_app/app.py db migrate
+flask --app dog_classifier_app/app.py db upgrade
