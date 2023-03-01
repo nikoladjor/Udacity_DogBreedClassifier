225f605ece622069e52548e6362626d7de9a09cc
Nikola Dordevic <nikoladjor@gmail.com>
2023-03-01 20:46:36 +0100

    changes in API, added log-in features

--- dog_classifier_app/app.py
+++ dog_classifier_app/app.py
@@ -7,12 +7,16 @@ from flask import render_template, request
 # Figure out the forms
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
-from wtforms import StringField, EmailField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, EmailField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, EqualTo
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
+
 from werkzeug.utils import secure_filename
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from .mlsrc.classification import dog_breed_clasifier, get_model
 import secrets
@@ -38,7 +42,7 @@ app = Flask(__name__, template_folder='templates/', instance_relative_config=Tru
 app._NETWORK = "Inception"
 app.secret_key = "VERY_BAD_SECRET_KEY"
 app.default_model = get_model(network=app._NETWORK, path_to_models=Path(__file__).parent / "mlsrc/saved_models")
-# Mimic the database
+# Mimic the database --> will be deprecated soon
 app._db = pd.DataFrame()
 Bootstrap(app)
 
@@ -52,21 +56,44 @@ app.config.from_mapping(
 db = SQLAlchemy(app=app)
 migrate = Migrate(app, db)
 
+# Flask Login Manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
 # Create User model
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200), nullable=False)
+    username = db.Column(db.String(200), nullable=False)
     email_address = db.Column(db.String(100), nullable=False, unique=True)
     date_created = db.Column(db.DateTime, default=datetime.now)
+    password_hash = db.Column(db.String(256))
     images = db.relationship('Image', backref='user', lazy=True)
+    
+    # password utils
+    @property
+    def password(self):
+        raise AttributeError("Not allowed to read the password!")
+    
+    @password.setter
+    def password(self, pwd):
+        self.password_hash = generate_password_hash(pwd)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self) -> str:
-        return f"User: <{self.name}>, created on <{self.date_created}>"
+        return f"User: <{self.username}>, created on <{self.date_created}>"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 # Create Model to store image information
 class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    image_path = db.Column(db.String(512))
+    image_path = db.Column(db.String(128))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     predictions = db.relationship('BreedPrediction', backref='image', lazy=True)
     def __repr__(self) -> str:
@@ -84,9 +111,20 @@ class BreedPrediction(db.Model):
 
 
 # Forms
+
+# Create Login Form
+class LoginForm(FlaskForm):
+	username = StringField("Username", validators=[DataRequired()])
+	password = PasswordField("Password", validators=[DataRequired()])
+	submit = SubmitField("Submit")
+
 class UserForm(FlaskForm):
-    name = StringField(label="User name", validators=[DataRequired()])
+    username = StringField(label="Username", validators=[DataRequired()])
     email_address = EmailField(label="Enter email address", validators=[DataRequired()])
+    password = PasswordField('Password', validators=[
+        DataRequired(), EqualTo('password_confirm', message="Passwords must match!")
+    ])
+    password_confirm = PasswordField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField("Submit")
 
 class ImageUploadForm(FlaskForm):
@@ -114,10 +152,40 @@ except OSError:
 @app.route('/')
 def home():
     return render_template("index.html")
-# a simple page that says hello
+
 @app.route('/cnn_explained')
 def training_description():
     return render_template("cnn_explained.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            # Auth the user here
+            if check_password_hash(user.password_hash, form.password.data):
+                # OK to login
+                login_user(user)
+                flash(f"User {form.username.data} logged in successfully!")
+            else:
+                flash("Wrong password!")
+        else:
+            # No user -- report error
+            flash("No user with that username, please try again.")
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    try:
+        logout_user()
+        flash("User Logged Out!")
+        return redirect(url_for('login'))
+    except:
+        flash("Error during logout")
+        return redirect(url_for('home'))
+    
 
 @app.route('/create_session', methods=["GET", "POST"])
 def create_session():
@@ -225,25 +293,49 @@ def process_image_file():
 
 @app.route('/user/add', methods=['GET', 'POST'])
 def add_user():
-    name = None
+    username = None
     form = UserForm()
     # Validate the form
     if form.validate_on_submit():
         usr_query = User.query.filter_by(email_address=form.email_address.data).first()
         if not usr_query:
-            user = User(name=form.name.data, email_address=form.email_address.data)
+            # User not found, create one
+            user = User(username=form.username.data, email_address=form.email_address.data, password=form.password.data)
             db.session.add(user)
             db.session.commit()
 
-        name = form.name.data
-        form.name.data = ''
+        username = form.username.data
+        form.username.data = ''
         form.email_address.data = ''
         flash("User Added!")
     
     all_users = User.query.order_by(User.date_created)
-    return render_template("add_user.html", name=name, form=form, all_users=all_users)
+    return render_template("add_user.html", username=username, form=form, all_users=all_users)
+
+@app.route('/user/update/<int:id>')
+def update_user(id):
+    return "This should update user"
+
+@app.route('/user/delete/<int:id>')
+def delete_user(id):
+    user_to_delete = User.query.get_or_404(id)
+    form = UserForm()
+    name = None
+    try:
+        db.session.delete(user_to_delete)
+        db.session.commit()
+        flash("user deleted")
+        all_users = User.query.order_by(User.date_created)
+        # return render_template("add_user.html", name=name, form=form, all_users=all_users)
+        return redirect(url_for('add_user', name=name, form=form, all_users=all_users))
+
+    except:
+        flash("ERROR! There was a problem deleting user")
+        return render_template("add_user.html", name=name, form=form, all_users=all_users)
+
 
 @app.route('/file/upload', methods=['GET', 'POST'])
+@login_required
 def file_uploader():
     f = None
     form = ImageUploadForm()
